f4380abd33aea6fbce5389b590274c83450a6682
JRagone <johnragone@me.com>
2018-05-14 12:38:24 -0400

    Deleted ClimbingOutput.html and Output.html
    Added for each loop that iterates over a list of subreddits and creates
    a page for each
    
    More information printed to console and formatted with new line between
    subreddits

--- src/package/Main.py
+++ src/package/Main.py
@@ -14,36 +14,48 @@ env = Environment(
     autoescape=select_autoescape(['html'])
 )
 
-#Get data from web page
-climbing_url = requests.get('https://www.reddit.com/r/climbing/.json')
+#List of subreddits to visit
+subreddits = ['climbing', 'MachineLearning', 'SpaceX']
 
-#If there is an error, report it
-if(not climbing_url.ok):
-    print("Error " + str(climbing_url.status_code))
-    #If the error is not a too_many_requests error, raise the error
-    if(climbing_url.status_code != 429):
-        print(climbing_url.raise_for_status())
-
-#Request data until returned
-while(not climbing_url.ok):
-    #If the error is not a too_many_requests error, raise the error
-    if(climbing_url.status_code != 429):
-        print(climbing_url.raise_for_status())
-    climbing_url = requests.get('https://www.reddit.com/r/climbing/.json')
-
-
-print("Web data retrieved.")
-#Decode JSON
-climbing_json = climbing_url.json()
-
-#Store list of posts
-posts_list = climbing_json["data"]["children"]
-
-#Template Description: This loops through each post
-# and adds links to titles by accessing .data.url and .data.title
-template = env.get_template('Page.html')
-#Use template to read and write data to Output.html
-with open("Output.html", "w") as output_file:
-    output_file.write(template.render(posts_list = posts_list))
+#Loop through subreddits
+for subreddit in subreddits:
+    #Get data from web page
+    url = requests.get('https://www.reddit.com/r/' + subreddit + '/.json')
+    
+    #If there is an error, report it
+    if(not url.ok):
+        print("Error " + str(url.status_code) + " from " + subreddit + " subreddit.")
+        #If the error is not a too_many_requests error, raise the error
+        if(url.status_code != 429):
+            print(url.raise_for_status())
+    
+    #Request data until returned
+    while(not url.ok):
+        #If the error is not a too_many_requests error, raise the error
+        if(url.status_code != 429):
+            print(url.raise_for_status())
+        url = requests.get('https://www.reddit.com/r/climbing/.json')
+    
+    print("Web data retrieved for " + subreddit + " subreddit.")
+    #Decode JSON
+    json = url.json()
+    
+    #Store list of posts
+    posts_list = json["data"]["children"]
+    
+    #Template Description: This loops through each post
+    # and adds links to titles by accessing .data.url and .data.title
+    template = env.get_template('Page.html')
+    #Use template to read and write data to Output.html
+    with open(subreddit + '.html', "w") as output_file:
+        output_file.write(template.render(posts_list = posts_list))
+    
+    #Check that file is closed
+    if(output_file.closed == False):
+        output_file.close()
+    print(subreddit + " file closed: " + str(output_file.closed))
+    
+    #New line between subreddits
+    print("\n")
     
 print("Complete.")
