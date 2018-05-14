dd2b9c874f98b216bbe68768c3f7eba66a6753a7
JRagone <johnragone@me.com>
2018-05-14 00:23:30 -0400

    Reports error code and raises error if it is not 429
    Added while loop to continue requests until data is retrieved
    
    New code commented

--- src/package/Main.py
+++ src/package/Main.py
@@ -6,7 +6,7 @@ Created on May 11, 2018
 
 #Libraries
 import requests
-from jinja2 import Template, Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 #Create environment
 env = Environment(
@@ -15,10 +15,27 @@ env = Environment(
 )
 
 #Get data from web page
-climbing_json = requests.get('https://www.reddit.com/r/climbing/.json').json()
+climbing_url = requests.get('https://www.reddit.com/r/climbing/.json')
 
-#Test printing title of second post
-print(climbing_json["data"]["children"][1]["data"]["title"])
+#If there is an error, report it
+if(not climbing_url.ok):
+    print("Error " + str(climbing_url.status_code))
+    #If the error is not a too_many_requests error, raise the error
+    if(climbing_url.status_code != 429):
+        print(climbing_url.raise_for_status())
+
+#Request data until returned
+while(not climbing_url.ok):
+    #If the error is not a too_many_requests error, raise the error
+    if(climbing_url.status_code != 429):
+        print(climbing_url.raise_for_status())
+    climbing_url = requests.get('https://www.reddit.com/r/climbing/.json')
+
+
+print("Web data retrieved.")
+#Decode JSON
+climbing_json = climbing_url.json()
+
 #Store list of posts
 posts_list = climbing_json["data"]["children"]
 
@@ -28,5 +45,3 @@ template = env.get_template('Page.html')
 #Use template to read and write data to Output.html
 with open("Output.html", "w") as output_file:
     output_file.write(template.render(posts_list = posts_list))
-
-
